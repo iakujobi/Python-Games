d0cfede2d6e3f905478976c69ef64222af938df1
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-08-01 23:33:02 -0400

    added the guess the number (CPU) file

---
+++ Games/guessTheNumber_CPU.py
@@ -0,0 +1,19 @@
+# Computer comes up with a random number and the user guesses the number
+import random  # library that allows CPU to come up with a random numbers
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        # continue guessing until you find the correct number
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+
+    print(f"Yay, congrats. You have guessed the number {random_number} correctly!!")
+
+
+guess(10)
